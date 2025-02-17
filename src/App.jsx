b2f76a241ee0e6aa5e5f8ea4a4e37986821e2ee6
razu381 <sirazu52@gmail.com>
2025-02-17 11:19:23 +0600

    firebase with env test

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,14 @@ function App() {
       <h1 class="text-3xl font-bold text-green-800">
         Hello world! This is Employee management system
       </h1>
+      <img
+        src="https://www.pixelstalk.net/wp-content/uploads/2016/06/Nature-Wallpaper.jpg"
+        alt=""
+      />
+      <img
+        src="https://wallpaperheart.com/wp-content/uploads/2018/03/beautiful-nature-wallpaper-Garden-HD-Wallpaper.jpg"
+        alt=""
+      />
     </div>
   );
 }
