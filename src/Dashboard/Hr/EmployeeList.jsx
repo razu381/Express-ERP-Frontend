60dc13c2a976ba7e8e8a043213363f6d0fed0ed5
razu381 <sirazu52@gmail.com>
2025-02-25 22:41:07 +0600

    work progress for hr

---
+++ src/Dashboard/Hr/EmployeeList.jsx
@@ -0,0 +1,368 @@
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
+import { IoShieldCheckmarkSharp } from "react-icons/io5";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import { FaRegTimesCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Loader from "../../Components/shared/Loader";
+
+function EmployeeList() {
+  let axiosSecure = useAxiosSecure();
+  //let userRole = useCheckRole();
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 5,
+  });
+  const [modalIsOpen, setIsOpen] = useState(false);
+  let [currColumnData, setCurrColumnData] = useState({});
+  //console.log(currColumnData);
+
+  let {
+    refetch,
+    isLoading,
+    isError,
+    data: employees = [],
+  } = useQuery({
+    queryKey: ["employee"],
+    queryFn: async () => {
+      let result = await axiosSecure.get("/employees-hr");
+      return result.data;
+    },
+  });
+  console.log(employees);
+
+  function handleVerification(email, isVerified) {
+    let changedVerification = !isVerified;
+    console.log("changed", changedVerification);
+    axiosSecure
+      .patch(`/users/${email}`, { isVerified: changedVerification })
+      .then((res) => {
+        if (res.data.modifiedCount > 0) {
+          toast.success("User verification status updated successfully");
+        }
+        refetch();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleHrPayment(e) {
+    e.preventDefault();
+    let salary = parseFloat(e.target.salary.value);
+    let month = e.target.month.value;
+    let year = e.target.year.value;
+    let email = currColumnData?.email;
+    let name = currColumnData?.name;
+    let paymentDetail = {
+      salary,
+      month,
+      year,
+      email,
+      name,
+      isPaidByAdmin: false,
+      paidByAdminDate: null,
+      transactionId: null,
+    };
+
+    //console.log(paymentDetail);
+    axiosSecure
+      .post("/payment-by-hr", paymentDetail)
+      .then((res) => {
+        if (res.data.insertedId) {
+          toast.success("Payment sent to admin for approval");
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  const columnHelper = createColumnHelper();
+
+  const columns = [
+    columnHelper.accessor("name", {
+      cell: (info) => info.getValue(),
+      header: () => <span className="flex items-center  gap-2">Name</span>,
+    }),
+    columnHelper.accessor("email", {
+      cell: (info) => (
+        <span className="italic text-blue-600">{info.getValue()}</span>
+      ),
+      header: () => <span className="flex items-center  gap-2">Email</span>,
+    }),
+    columnHelper.accessor("isVerified", {
+      cell: (info) => {
+        {
+          let verified = info.getValue();
+
+          return (
+            <button
+              onClick={() =>
+                handleVerification(info.row.original?.email, verified)
+              }
+            >
+              {verified ? (
+                <IoShieldCheckmarkSharp color="green" size={25} />
+              ) : (
+                <FaRegTimesCircle color="red" size={25} />
+              )}
+            </button>
+          );
+        }
+      },
+      header: () => <span className="flex items-center  gap-2">Verified</span>,
+    }),
+    columnHelper.accessor("bank_account", {
+      cell: (info) => info.getValue(),
+      header: () => (
+        <span className="flex items-center  gap-2">Bank Account</span>
+      ),
+    }),
+    columnHelper.accessor("salary", {
+      cell: (info) => info.getValue(),
+      header: () => <span className="flex items-center  gap-2">Salary</span>,
+    }),
+    columnHelper.display({
+      id: "pay",
+      cell: (info) => {
+        let isVerified = info.row.original.isVerified;
+
+        return (
+          <button
+            disabled={!isVerified}
+            className="btn bg-violet-700 text-white"
+            onClick={() => {
+              setIsOpen(true);
+              setCurrColumnData(info.row.original);
+            }}
+          >
+            Pay
+          </button>
+        );
+      },
+      header: () => <span className="flex items-center gap-2">Edit</span>,
+    }),
+    columnHelper.display({
+      id: "details",
+      cell: (info) => (
+        <Link
+          className="btn"
+          to={`/dashboard/details/${info.row.original?.email}`}
+        >
+          Details
+        </Link>
+      ),
+      header: () => <span className="flex items-center  gap-2">Delete</span>,
+    }),
+  ];
+
+  const table = useReactTable({
+    data: employees,
+    columns,
+    state: { pagination },
+    onPaginationChange: setPagination,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <div>Error loading employee data</div>;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center mx-[3%]">
+      <h2 className="text-center py-5  text-3xl font-bold">Tanstack Table</h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table table-zebra w-full">
+          <thead className=" text-white">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th key={header.id} className="py-3 px-5 text-black">
+                      <div className=" mb-2">
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </div>
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className="py-3 px-5">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="flex justify-between">
+          <div className="pagination-controls flex gap-3">
+            <button
+              onClick={() => table.firstPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              {"<<"}
+            </button>
+            <button
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+              className="text-blue-600 font-bold"
+            >
+              {"<"}
+            </button>
+            <span>
+              Page {table.getState().pagination.pageIndex + 1} of{" "}
+              {table.getPageCount()}
+            </span>
+            <button
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+              className="text-blue-600 font-bold"
+            >
+              {">"}
+            </button>
+            <button
+              onClick={() => table.lastPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              {">>"}
+            </button>
+          </div>
+
+          {/* Step 5: Page Size Selector */}
+          <div>
+            <select
+              value={table.getState().pagination.pageSize}
+              onChange={(e) => table.setPageSize(Number(e.target.value))}
+            >
+              {[5, 10, 20, 30, 40, 50].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+      {/* Modal for edit work */}
+      <Modal
+        isOpen={modalIsOpen}
+        ariaHideApp={false}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={() => setIsOpen(false)}
+        // style={customStyles}
+        contentLabel="Example Modal"
+        // className="mx-28 bg-red-700"
+      >
+        <div>
+          <button
+            onClick={() => setIsOpen(false)}
+            className="btn flex flex-col ml-auto"
+          >
+            close
+          </button>
+          <h2 className="text-center font-bold ">
+            <form onSubmit={handleHrPayment}>
+              <div className="flex flex-col md:flex-row gap-2 mb-5 justify-between">
+                <div className="flex-1">
+                  <div className="label mb-2">
+                    <span className="label-text">Salary</span>
+                  </div>
+                  <input
+                    defaultValue={currColumnData.salary}
+                    disabled
+                    type="number"
+                    placeholder="Salary"
+                    className="input input-bordered w-full !text-black !border-gray-100"
+                    name="salary"
+                  />
+                </div>
+                <div className="flex-1">
+                  <div className="label mb-2">
+                    <span className="label-text">Month</span>
+                  </div>
+                  <select
+                    required
+                    className="select select-bordered w-full"
+                    name="month"
+                    defaultValue="January"
+                  >
+                    <option value="January">January</option>
+                    <option value="February">February</option>
+                    <option value="March">March</option>
+                    <option value="April">April</option>
+                    <option value="May">May</option>
+                    <option value="June">June</option>
+                    <option value="July">July</option>
+                    <option value="August">August</option>
+                    <option value="September">September</option>
+                    <option value="October">October</option>
+                    <option value="November">November</option>
+                    <option value="December">December</option>
+                  </select>
+                </div>
+                <div className="flex-1">
+                  <div className="label mb-2">
+                    <span className="label-text">Year</span>
+                  </div>
+                  <select
+                    required
+                    className="select select-bordered w-full justify-center items-center"
+                    name="year"
+                    defaultValue="2025"
+                  >
+                    <option value="2025">2025</option>
+                    <option value="2026">2026</option>
+                    <option value="2027">2027</option>
+                    <option value="2028">2028</option>
+                    <option value="2029">2029</option>
+                    <option value="2030">2030</option>
+                    <option value="2031">2031</option>
+                    <option value="2032">2032</option>
+                    <option value="2033">2033</option>
+                    <option value="2034">2034</option>
+                    <option value="2035">2035</option>
+                    <option value="2036">2036</option>
+                    <option value="2037">2037</option>
+                    <option value="2038">2038</option>
+                    <option value="2039">2039</option>
+                    <option value="2040">2040</option>
+                  </select>
+                </div>
+              </div>
+              <input
+                type="submit"
+                value="Pay"
+                className="btn  bg-green-700  text-white w-full"
+              />
+            </form>
+          </h2>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export default EmployeeList;
---
+++ src/Dashboard/Hr/WorkProgress.jsx
@@ -0,0 +1,233 @@
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { FaUser } from "react-icons/fa";
+import Loader from "../../Components/shared/Loader";
+
+function WorkProgress() {
+  let axiosSecure = useAxiosSecure();
+  const [columnFilters, setColumnFilters] = useState([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 5,
+  });
+
+  let {
+    isLoading,
+    isError,
+    data: workListHR = [],
+  } = useQuery({
+    queryKey: ["AllWorkListHR"],
+    queryFn: async () => {
+      let result = await axiosSecure.get("/worksheet-hr");
+      return result.data;
+    },
+  });
+
+  console.log(workListHR);
+
+  function Filter({ column }) {
+    const { filterVariant } = column.columnDef.meta ?? {};
+    const columnFilterValue = column.getFilterValue();
+
+    let uniqueValues = Array.from(column.getFacetedUniqueValues().keys());
+
+    if (column.id === "date") {
+      uniqueValues = uniqueValues
+        .map((value) =>
+          new Date(value).toLocaleDateString(undefined, { month: "long" })
+        )
+        .filter((value, idx, arr) => arr.indexOf(value) === idx)
+        .sort();
+    } else {
+      uniqueValues = uniqueValues.sort();
+    }
+
+    if (filterVariant === "select") {
+      return (
+        <select
+          onChange={(e) => column.setFilterValue(e.target.value)}
+          value={columnFilterValue ?? ""}
+        >
+          <option value="">All</option>
+          {uniqueValues.map((value) => (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+      );
+    }
+
+    return null;
+  }
+
+  const columnHelper = createColumnHelper();
+
+  const columns = [
+    columnHelper.accessor("name", {
+      cell: (info) => info.getValue(),
+      header: () => <span className="flex items-center  gap-2">Name</span>,
+      meta: { filterVariant: "select" },
+    }),
+    columnHelper.accessor("date", {
+      cell: (info) => {
+        const iSODate = info.getValue();
+        const options = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        };
+
+        return (
+          <span>
+            {new Date(iSODate).toLocaleDateString(undefined, options)}
+          </span>
+        );
+      },
+      header: () => <span className="flex items-center  gap-2">Date</span>,
+      filterFn: (row, columnId, filterValue) => {
+        if (!filterValue) return true;
+        // Extract the month from the row's date.
+        const month = new Date(row.original.date).toLocaleDateString(
+          undefined,
+          { month: "long" }
+        );
+        return month === filterValue;
+      },
+      meta: { filterVariant: "select" },
+    }),
+
+    columnHelper.accessor("Tasks", {
+      cell: (info) => info.getValue(),
+      header: () => <span className="flex items-center  gap-2">Task</span>,
+    }),
+    columnHelper.accessor("hours", {
+      cell: (info) => info.getValue(),
+      header: () => <span className="flex items-center  gap-2">Hours</span>,
+    }),
+  ];
+
+  const table = useReactTable({
+    data: workListHR,
+    columns,
+    state: { columnFilters, pagination },
+    onPaginationChange: setPagination,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <div>Error loading employee data</div>;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center mx-[3%]">
+      <h2 className="text-center py-5  text-3xl font-bold">Work Progress</h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table table-zebra w-full">
+          <thead className=" text-white">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th key={header.id} className="py-3 px-5 text-black">
+                      <div className=" mb-2">
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </div>
+                      {header.column.getCanFilter() && (
+                        <Filter column={header.column} />
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className="py-3 px-5">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="flex justify-between">
+          <div className="pagination-controls flex gap-3">
+            <button
+              onClick={() => table.firstPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              {"<<"}
+            </button>
+            <button
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+              className="text-blue-600 font-bold"
+            >
+              {"<"}
+            </button>
+            <span>
+              Page {table.getState().pagination.pageIndex + 1} of{" "}
+              {table.getPageCount()}
+            </span>
+            <button
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+              className="text-blue-600 font-bold"
+            >
+              {">"}
+            </button>
+            <button
+              onClick={() => table.lastPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              {">>"}
+            </button>
+          </div>
+
+          {/* Step 5: Page Size Selector */}
+          <div>
+            <select
+              value={table.getState().pagination.pageSize}
+              onChange={(e) => table.setPageSize(Number(e.target.value))}
+            >
+              {[5, 10, 20, 30, 40, 50].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WorkProgress;
